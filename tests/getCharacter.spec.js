60f1b6925fd2932ddd73c2886aa22957e1eb3a7b
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-12 20:30:05 -0300

    requisito-9-concluido

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,42 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    /* fail('Teste vazio!'); */
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter('undefined')).toBeUndefined()
+  });
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+  it('retorna um objeto',() => {
+    expect(getCharacter('Arya')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    })
+  })
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    it('retorna um objeto',() => { 
+     expect(getCharacter('Brienne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+    })
+  }) 
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    it('retorna um objeto',() => {
+      expect(getCharacter('Melissandre')).toEqual({
+        name: 'Melissandre',
+        class: 'Necromancer',
+        phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+      })
+    })  
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    it('teste case sensitive',() => {
+      expect(getCharacter('case sensitive')).not.toBe('Arya','Brianne','Melissandre')
+    })
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    it('retorna undefined',() => {
+      expect(getCharacter('nome')).not.toBe('undefined')
   });
 });
